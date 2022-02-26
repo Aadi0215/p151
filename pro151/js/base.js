3a8f944707fa606f6da5aaca1062f9623480d5b0
Aadi0215 <72636522+Aadi0215@users.noreply.github.com>
2022-02-26 17:06:35 -0500

    Add files via upload

---
+++ pro151/js/base.js
@@ -0,0 +1,14 @@
+AFRAME.registerComponent("base", {
+	schmea: {
+        radius:{type:'number', default:150},
+        height:{type:'number', default:2.3}
+		},
+		init: function () {
+			this.el.setAttribute("geometry",{
+                primitive:'cylinder',
+                radius:this.data.radius,
+                height:this.data.height
+            });
+		},
+    },
+);
